4d36caeed513e226568f5708c5307d5b29f68f80
André Calero Valdez <andrecalerovaldez@gmail.com>
2024-05-17 13:04:16 +0200

    fixed a bug

--- summer.rb
+++ summer.rb
@@ -5,6 +5,6 @@ APPOINTMENTS_BY_WDAY = [
   {10 => 'OOO Jork Milde', 12 => 'OOO Lilian', 13 => 'OOO Hendrik', 14 => 'OOO Marthe', 16 => 'OOO Marvin & Maged'},
   {10 => 'SMNF', 11 => 'SMNF', 12 => 'OSE', 13 => 'OSE', 14 => 'OOO Toni', 16 => 'JourFixe'}, # Wednesday
   {11 => 'OOO Leonard', 14 => 'Coffe Break', 15 => 'OOO Lilian'},
-  {11 => 'Diss-Sprechstunde', 13 = 'Lehre-Prep', 16 => 'Wrapup'}, # Friday
+  {11 => 'Diss-Sprechstunde', 13 => 'Lehre-Prep', 16 => 'Wrapup'}, # Friday
   {},
 ]
